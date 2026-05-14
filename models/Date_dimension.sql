6ea344910c96caf2122f132a4185457f0893b142
Kunal Memne <kunalmemne9111@gmail.com>
2026-05-14 20:15:11 +0000

    daily_weather model created

--- models/Date_dimension.sql
+++ models/Date_dimension.sql
@@ -10,18 +10,9 @@ CASE
 WHEN DAYNAME(TO_TIMESTAMP(started_at))  IN ('Sat' , 'Sun')
 then 'WEEKEND'
 ELSE 'WEEKDAY' 
-END AS DAY_TYPE 
+END AS DAY_TYPE ,
 
-,
-CASE 
-WHEN MONTH (TO_TIMESTAMP(started_at)) IN (12 ,1 ,2 )
-THEN 'WINTER'
-WHEN MONTH (TO_TIMESTAMP(started_at)) IN (3 ,4 ,5 )
-THEN 'SPRING'
-WHEN MONTH (TO_TIMESTAMP(started_at)) IN (6 ,7 ,8 )
-THEN 'SUMMER'
-ELSE 'AUTUMN'
-END AS STATION_YEAR
+{{get_season('STARTED_AT')}} AS STATION_YEAR
 
 FROM 
 {{ source('demo', 'bike') }}
